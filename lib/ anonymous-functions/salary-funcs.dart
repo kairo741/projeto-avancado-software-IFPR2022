f21d7844f1444aecabad15c36bcf7cc8d0008cd2
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-04-20 20:51:58 -0300

    Inicio e explicação alterada

--- lib/ anonymous-functions/salary-funcs.dart
+++ lib/ anonymous-functions/salary-funcs.dart
@@ -1,18 +1,21 @@
-/**
-Atividade I
-1). Qual a diferença entre a 1º e a 2º forma? Na sua opinião, o que melhorou
-e o que piorou?
-2). O que é e para que serve parâmetros do tipo função? Em que caso utilizamos?
+///Atividade II - Defina 3 funções do mesmo contexto e uma interface para estas
+/// funções
+/// → função para escolha das funções do Salário
+/// → Receber salario e um valor. Bonificar e retornar o valor do salário.
+/// → Receber salario e um valor. Descontar e retornar o valor do salário.
+/// → Receber salario e um valor. Neste caso o valor é o bônus que tem encargos de 10%.
+/// Adicionar o bônus e retornar o valor do salário.
+///
+/// Atividade III
+/// Pensar em um projeto único
+/// Fazer um exemplo seu
 
-Atividade II - Defina 3 funções do mesmo contexto e uma interface para estas
-funções
-→ função para escolha das funções do Salário
-→ Receber salario e um valor. Bonificar e retornar o valor do salário.
-→ Receber salario e um valor. Descontar e retornar o valor do salário.
-→ Receber salario e um valor. Neste caso o valor é o bônus que tem encargos de 10%.
-Adicionar o bônus e retornar o valor do salário.
+const String menu = """
+1 → Receber salario e um valor. Bonificar e retornar o valor do salário.
+2 → Receber salario e um valor. Descontar e retornar o valor do salário.
+3 → Receber salario e um valor. Neste caso o valor é o bônus que tem encargos de 10%.
+""";
 
-Atividade III
-Pensar em um projeto único
-Fazer um exemplo seu
-*/
+void showInterface() {
+  print(menu);
+}
