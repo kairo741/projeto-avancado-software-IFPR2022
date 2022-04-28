0f274f91fca192506c278c7df807c21cff78a0a9
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-04-27 22:48:28 -0300

    Implementação da Atividade III com os exercícios A, B e C

--- lib/class-27-04-22/main_exerc_3.dart
+++ lib/class-27-04-22/main_exerc_3.dart
@@ -13,10 +13,31 @@ void main() {
   validateNumber(10, isPrime);
 
   /// b) Função que solicite a data inicial e retorne quanto tempo passou em: (1) anos; (2) dias; e (3) horas.
+  printTime(DateTime.now().subtract(Duration(days: 9000)), DateTime.now(),
+      (DateTime initDate, DateTime endDate) {
+    // var year0 = DateTime(0);
+    // year0.add(Duration(days: initDate.difference(endDate).inDays));
+    return "A diferença em anos é de ${(endDate.difference(initDate).inDays / 365).toStringAsFixed(2)}";
+  });
+
+  printTime(DateTime.now().subtract(Duration(days: 500)), DateTime.now(),
+      (DateTime initDate, DateTime endDate) {
+    return "A diferença em dias é de ${endDate.difference(initDate).inDays}";
+  });
+
+  printTime(DateTime.now().subtract(Duration(days: 50)), DateTime.now(),
+      (DateTime initDate, DateTime endDate) {
+    return "A diferença em horas é de ${endDate.difference(initDate).inHours}";
+  });
+
   /// c) Função que solicite a quantidade de dias trabalhadas, valor que recebe por hora e calcule o salarário para: (1) auxiliar; (2) júnior; e (3) pleno.
+  workedHours(10, 200, (int workedDays, double hourWage){
+    int hours = workedDays * 8;
+  });
   /// Exercício 02 - para cada função "hospedeira", defina uma função anônima.
 }
 
+//region Exercício 01 - a)
 bool isPrime(int value) {
   for (var i = 2; i <= value / i; ++i) {
     if (value % i == 0) {
@@ -30,3 +51,22 @@ validateNumber(int value, Function(int value) validate) {
   bool isValid = validate(value);
   print(isValid ? "Valido" : "Inválido");
 }
+//endregion
+
+//region Exercício 01 - b)
+printTime(
+    DateTime initDate, DateTime endDate, Function(DateTime initDate, DateTime endDate) validate) {
+  String dateDifference = validate(initDate, endDate);
+  print(dateDifference);
+}
+
+//endregion
+
+//region Exercício 01 - c)
+workedHours(
+    int workedDays, double hourWage, Function(int workedDays, double hourWage) calcSalary) {
+  double fullSalary = calcSalary(workedDays, hourWage);
+  print(fullSalary);
+}
+
+//endregion
