4750c205aadd356b4929ea41507c0392b468f25e
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-05-04 18:24:24 -0300

    Exercício 01 - c)

--- lib/class-27-04-22/main_exerc_3.dart
+++ lib/class-27-04-22/main_exerc_3.dart
@@ -1,4 +1,5 @@
 void main() {
+  print('\x1B[35m---------Exercício 01 - a)---------\x1B[0m');
   /// Exercício 01 - implemente as funções
   /// a) Função que solicite um número e realizar validações: (1) se é par; (2) maior que zero; (3) se é um número primo.
   /// função "hospedeira" : solicitar um número
@@ -12,6 +13,7 @@ void main() {
   //(3)
   validateNumber(10, isPrime);
 
+  print('\x1B[35m---------Exercício 01 - b)---------\x1B[0m');
   /// b) Função que solicite a data inicial e retorne quanto tempo passou em: (1) anos; (2) dias; e (3) horas.
   printTime(DateTime.now().subtract(Duration(days: 9000)), DateTime.now(),
       (DateTime initDate, DateTime endDate) {
@@ -30,10 +32,39 @@ void main() {
     return "A diferença em horas é de ${endDate.difference(initDate).inHours}";
   });
 
-  /// c) Função que solicite a quantidade de dias trabalhadas, valor que recebe por hora e calcule o salarário para: (1) auxiliar; (2) júnior; e (3) pleno.
-  workedHours(10, 200, (int workedDays, double hourWage){
-    int hours = workedDays * 8;
-  });
+  print('\x1B[35m---------Exercício 01 - c)---------\x1B[0m');
+  /// c) Função que solicite a quantidade de dias trabalhadas, valor que recebe por hora e calcule o salário para: (1) auxiliar; (2) junior; e (3) pleno.
+  ///
+  /// (1) auxiliar: 6 horas diárias de trabalho e não pode fazer hora extra
+  /// (2) junior: 8 horas diárias de trabalho e trabalha aos sábados recebendo 0.5x
+  /// a mais do salário por hora no 6° dio de trabalho (sábado)
+  /// (3) pleno: 8 horas diárias de trabalho, trabalha aos sábados e a cada 30 dias
+  ///  recebe uma bonificação em dinheiro de 0.7x baseado no salário mensal
+  workedHours((int workedDays, double hourWage, WorkerRole role) {
+    switch (role) {
+      case WorkerRole.auxiliar:
+        return (hourWage * 6) * workedDays;
+      case WorkerRole.junior:
+        var wageOfADay = (hourWage * 8);
+        if (workedDays > 6) {
+          var workedSats = workedDays / 6;
+          var workingDays = workedDays - workedSats;
+          return (wageOfADay * workingDays) + ((workedSats * wageOfADay) * 1.5);
+        } else {
+          return wageOfADay * workedDays;
+        }
+      case WorkerRole.pleno:
+        var wageOfADay = (hourWage * 8);
+        if (workedDays > 24) {
+          var workedMonths = workedDays / 24;
+          var monthWage = 24 * wageOfADay;
+          return (wageOfADay * workedDays) + ((monthWage * workedMonths) * 1.7);
+        } else {
+          return wageOfADay * workedDays;
+        }
+    }
+  }, hourWage: 5.5, workedDays: 22, role: WorkerRole.junior);
+
   /// Exercício 02 - para cada função "hospedeira", defina uma função anônima.
 }
 
@@ -63,10 +94,16 @@ printTime(
 //endregion
 
 //region Exercício 01 - c)
-workedHours(
-    int workedDays, double hourWage, Function(int workedDays, double hourWage) calcSalary) {
-  double fullSalary = calcSalary(workedDays, hourWage);
-  print(fullSalary);
+workedHours(Function(int workedDays, double hourWage, WorkerRole role) calcSalary,
+    {required int workedDays, required double hourWage, required WorkerRole role}) {
+  double fullSalary = calcSalary(workedDays, hourWage, role);
+  print("O salário total do ${role.name} é de R\$${fullSalary.toStringAsFixed(2)}");
+}
+
+enum WorkerRole {
+  auxiliar,
+  junior,
+  pleno,
 }
 
 //endregion
