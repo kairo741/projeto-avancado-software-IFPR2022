a823719a0f9fd9f962dd926648a1e45ff93682cb
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-05-04 22:48:44 -0300

    Sábado Letivo
    Comentário com a descrição da atividade do sábado letivo

---
+++ lib/saturday-30-04-22/main.dart
@@ -0,0 +1,16 @@
+/// 1)
+/// a) O que é e para que serve tratamento de erros?
+/// b) Quais são as formas de tratamentos de erros?
+/// c) Qual a difereça entre erro e exceção?
+/// d) Qual a diferença entre excessão implícita (contornável) e explícita (incontornável)?
+/// e) O que são: (1) throwables; (2) Checked Exception; e (3) Unchecked (Runtime)?
+/// ------------------------------------------------------------------------------------
+/// 2)
+/// a) Quais são os tratamentos de erros existentes no dart?
+/// b) Quais são as principais excessões existentes no dart?
+/// c) O que é para que serve o bloco try-catch? O que é e como utilizamos o objeto de exceção?
+/// O que é e como utilizamos o objeto Stack trace?
+/// d) Explique e exemplifique o uso das cláusulas on e finally no bloco try-cath.
+/// f) Implemente o try-catch (com relevância) em nas funções do seu projeto.
+/// g) É possível criar exceções específicas? Se sim, em que caso podem ser utilizas?
+/// h) analise o seu projeto e crie exceções específicas que facilite a localizção e a manutenção do projeto.
