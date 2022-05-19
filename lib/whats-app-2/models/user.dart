693244e4f9a074c80d8f802220272eb47a65eede
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-05-18 22:46:46 -0300

    Novo atributo
    Novo atributo age que armazena a idade (data de nascimento) do usuário e premium que diz se o usuário é premium ou não

--- lib/whats-app-2/models/user.dart
+++ lib/whats-app-2/models/user.dart
@@ -1,11 +1,13 @@
 class User {
   String name;
   DateTime age;
+  bool isPremium;
   bool active;
 
   User({
     required this.name,
     required this.age,
+    this.isPremium = false,
     this.active = true,
   });
 }
